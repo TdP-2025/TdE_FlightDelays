f8b6e78b8cea8fa59c4b7d0c20d9573d49d6abe0
Giuseppe Averta <g.averta3@gmail.com>
2025-05-12 11:28:27 +0200

    Lezione del 12/05/2025

--- database/DAO.py
+++ database/DAO.py
@@ -21,3 +21,28 @@ class DAO():
         cursor.close()
         conn.close()
         return result
+
+    @staticmethod
+    def getAllNodes(nMin, idMapAirports):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """SELECT t.ID, t.IATA_CODE, count(*) as N
+                    FROM (SELECT a.ID, a.IATA_CODE , f.AIRLINE_ID, count(*)
+                    FROM airports a, flights f 
+                    WHERE a.ID = f.ORIGIN_AIRPORT_ID or a.ID = f.DESTINATION_AIRPORT_ID 
+                    GROUP BY a.ID, a.IATA_CODE , f.AIRLINE_ID) t
+                    GROUP BY t.ID, t.IATA_CODE
+                    having N >= %s
+                    order by N asc"""
+
+        cursor.execute(query, (nMin,))
+
+        for row in cursor:
+            result.append(idMapAirports[row["ID"]])
+
+        cursor.close()
+        conn.close()
+        return result
